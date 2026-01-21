4c5ed5d41bad9b321b867d6069e46e03bdde4bff
prazim-projects <managervonxlique@gmail.com>
2026-01-21 21:21:43 +0300

    remove-messed-backend

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,10 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.leab.android',
   appName: 'travelNotes',
-  webDir: 'dist'
+  webDir: 'dist',
+  // bundledWebRuntime: false
 };
 
 export default config;
