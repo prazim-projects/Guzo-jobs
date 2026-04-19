78329ba05c6cc12c3cb53796bb24a958ba86a34b
prazim-projects <managervonxlique@gmail.com>
2026-04-19 14:55:04 +0300

    chore(mobile): capacitor/android id and gradle alignment

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.leab.android',
+  appId: 'com.leab.guzo',
   appName: 'guzoJobs',
   webDir: 'dist',
   // bundledWebRuntime: false
